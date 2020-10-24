072ddc7739b1388c87691f3f12b58df0d210edc7
carsten-sc <carsten@visions-ware.de>
2020-10-24 17:15:49 +0200

    fixed error with command

--- exchange/carbon-feeder.sh
+++ exchange/carbon-feeder.sh
@@ -10,5 +10,5 @@ fi
 if pgrep -f "carbon-feeder.py" &>/dev/null; then
   echo "carbon-feeder it is already running"
 else
-  nohup python3 ./carbon-feeder.py > carbon-feeder.output &>/dev/null &
+  nohup bash -c "./carbon-feeder.py > .carbon-feeder.output" &>/dev/null &
 fi
