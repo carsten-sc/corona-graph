4f4c2f5ef545207193630a22d9725985a524cf0a
carsten-sc <carsten@visions-ware.de>
2020-10-24 18:41:15 +0200

    removed bash -c

--- exchange/carbon-feeder.sh
+++ exchange/carbon-feeder.sh
@@ -5,5 +5,5 @@ cd /exchange
 if pgrep -f "carbon-feeder.py" &>/dev/null; then
   echo "carbon-feeder it is already running"
 else
-  nohup bash -c "./carbon-feeder.py > logs/carbon-feeder.output" &>/dev/null &
+  ./carbon-feeder.py > logs/carbon-feeder.output &
 fi
